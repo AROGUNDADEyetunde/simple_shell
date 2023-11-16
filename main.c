e0a3787a06bb672358cb9678b9f81e716db8ca87
AROGUNDADEyetunde <arogundadeyetunde1994@gmail.com>
2023-11-15 19:34:44 -0800

    0x16. C - Simple Shell

--- main.c
+++ main.c
@@ -1,83 +1,44 @@
 #include "shell.h"
 
 /**
- * free_data - Frees memory allocated for data_shell structure.
- * @datash: Pointer to the data_shell structure.
- * Return: void
- */
-void free_data(data_shell *datash)
-{
-	unsigned int i;
-
-	// Free environment variables
-	for (i = 0; datash->_environ[i]; i++)
-	{
-		free(datash->_environ[i]);
-	}
-
-	free(datash->_environ);
-	free(datash->pid);
-}
-
-/**
- * set_data - Initializes data_shell structure with environment variables.
- * @datash: Pointer to the data_shell structure.
- * @av: Arguments vector.
- * Return: void
- */
-void set_data(data_shell *datash, char **av)
-{
-	unsigned int i;
-
-	// Set data_shell members
-	datash->av = av;
-	datash->input = NULL;
-	datash->args = NULL;
-	datash->status = 0;
-	datash->counter = 1;
-
-	// Count environment variables
-	for (i = 0; environ[i]; i++)
-		;
-
-	// Allocate memory for environment variables
-	datash->_environ = malloc(sizeof(char *) * (i + 1));
-
-	// Copy environment variables
-	for (i = 0; environ[i]; i++)
-	{
-		datash->_environ[i] = _strdup(environ[i]);
-	}
-
-	datash->_environ[i] = NULL;
-	datash->pid = aux_itoa(getpid());
-}
-
-/**
- * main - Entry point for the shell program.
- * @ac: Number of arguments.
- * @av: Arguments vector.
- * Return: Shell exit status.
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
  */
 int main(int ac, char **av)
 {
-	data_shell datash;
-	(void) ac;
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-	// Set up signal handler for SIGINT
-	signal(SIGINT, get_sigint);
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
 
-	// Initialize data_shell structure
-	set_data(&datash, av);
-
-	// Run the shell loop
-	shell_loop(&datash);
-
-	// Free allocated memory
-	free_data(&datash);
-
-	// Return shell exit status
-	if (datash.status < 0)
-		return (255);
-	return (datash.status);
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
